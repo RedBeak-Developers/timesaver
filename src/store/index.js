1cb6cfe34dfe8e3bd2ce138339e9bbd2225d7d0f
Alex Rougebec <arougebec22@gmail.com>
2024-06-26 12:04:30 -0400

    Small bug fix
    fixed middleware array bug

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ const chrome = typeof window.chrome !== 'undefined' ? window.chrome : mockChrome
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: thunk,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
 
 // Load timer settings from chrome.storage on startup
@@ -16,6 +16,7 @@ chrome.storage.sync.get(['focusTime', 'breakTime'], (result) => {
   const focusTime = result.focusTime || 25;
   const breakTime = result.breakTime || 5;
   store.dispatch(loadTimerSettings(focusTime, breakTime));
+  console.log("Loaded timer settings from startup");
 });
 
 // Subscribe to store changes to send Chrome messages
@@ -23,18 +24,21 @@ store.subscribe(() => {
   const state = store.getState();
   if (chrome.runtime && chrome.runtime.sendMessage) {
     if (state.timer.isRunning) {
-      chrome.runtime.sendMessage({ action: 'startFocus', focusTime: state.timer.focusTime, breakTime: state.timer.breakTime }, (response) => {
-        console.log(response.status);
-      });
+      chrome.runtime.sendMessage(
+        { action: 'startFocus', focusTime: state.timer.focusTime, breakTime: state.timer.breakTime },
+        (response) => {
+          console.log("Store subscribe: Timer is running:", response.status);
+        }
+      );
     } else {
       chrome.runtime.sendMessage({ action: 'stopFocus' }, (response) => {
-        console.log(response.status);
+        console.log("Store subscribe: Timer is stopped:", response.status);
       });
     }
   }
 });
 
-// Subscribe to changes in the timer state to update the UI
+// Listen for messages from the background script to update the store
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'updateTimerState') {
     store.dispatch(updateTimer(request.timerState));
