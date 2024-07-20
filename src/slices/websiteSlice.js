d7ad1c252a66daf9a5459b321dc217b8fde4fcdd
Alex Rougebec <arougebec22@gmail.com>
2024-07-20 01:44:34 -0400

    changed to slices

---
+++ src/slices/websiteSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  websites: [],
+};
+
+const websiteSlice = createSlice({
+  name: 'websites',
+  initialState,
+  reducers: {
+    addWebsite(state, action) {
+      state.websites.push(action.payload);
+    },
+    removeWebsite(state, action) {
+      state.websites = state.websites.filter(website => website !== action.payload);
+    },
+    loadWebsites(state, action) {
+      state.websites = action.payload;
+    },
+  },
+});
+
+export const { addWebsite, removeWebsite, loadWebsites } = websiteSlice.actions;
+export default websiteSlice.reducer;
